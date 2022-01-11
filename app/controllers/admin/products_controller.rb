3d0bfea1631778230c45c712a2a879c341bf68a2
vickyruud <aprajit.k@gmail.com>
2022-01-11 10:38:09 -0500

    add basic auth for admin page

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: ENV["AUTH_USERNAME"], password: ENV["AUTH_PASSWORD"]
 
   def index
     @products = Product.order(id: :desc).all
